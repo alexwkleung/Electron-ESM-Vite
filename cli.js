fe56ca4acbe227d8bcbe492c678fb4947e680066
Alex Leung <alexleung25@outlook.com>
2023-12-20 14:57:16 -0800

    Fix process import in cli

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 import chokidar from 'chokidar'
 import { exec } from 'child_process'
 import * as fs from 'fs'
-import { process } from 'process'
+import process from 'process'
 
 const chokidarPaths = [
     'src/electron/main.ts', 
